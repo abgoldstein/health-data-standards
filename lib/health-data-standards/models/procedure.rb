46dc7dcdb99fd2759609e9de91e96f7c5a2d6377
Adam B. Goldstein <abgoldstein@gmail.com>
2012-10-17 16:34:02 -0400

    Adding reason and source to Procedures.

--- lib/health-data-standards/models/procedure.rb
+++ lib/health-data-standards/models/procedure.rb
@@ -1,6 +1,9 @@
 class Procedure < Entry
   field :site,             type: Hash
   field :incisionDateTime, type: Integer
+
+  embeds_one :reason, class_name: "Entry"
+  embeds_one :source, class_name: "Entry"
   
   belongs_to :performer, class_name: "Provider"
 
